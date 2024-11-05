4e132b8c9480cf6fe546a89d35fcd5554309a1e0
jinpy0 <jinpyo0920@gmail.com>
2024-11-05 17:29:00 +0900

    이미지, 아이디 패널, 피드 패널 추가

--- src/FirstTest/UI/DiaryListScreen.java
+++ src/FirstTest/UI/DiaryListScreen.java
@@ -1,6 +1,7 @@
 package FirstTest.UI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DiaryListScreen extends JFrame {
     public DiaryListScreen() { // User 클래스의 인스턴스를 매개변수로 받아와서 리스트 만들기?
@@ -10,11 +11,42 @@ public class DiaryListScreen extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        // JPanel imagePanel 이미지 어떻게 받아올 지
 
 
+        // JPanel imagePanel 이미지 어떻게 받아올 지 (일단 빈 박스로 구현)
+        // 이미지, 회원 아이디 출력
+        // 이미지 추가
+        JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel imageLabel = new JLabel();
+        imageLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        imageLabel.setPreferredSize(new Dimension(100, 100));
+        imageLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        imagePanel.add(imageLabel);
+        // 아이디 추가
+        JLabel userIdLabel = new JLabel("사용자 ID");
+        userIdLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        imagePanel.add(userIdLabel);
 
 
+        // 사용자 피드
+        JPanel userFeed = new JPanel(new GridLayout(3, 3, 5, 5)); // 5,5는 간격 조절
+        for (int i = 0; i < 9; i++) {
+            JLabel feedLabel = new JLabel();
+            feedLabel.setPreferredSize(new Dimension(100, 100));
+            feedLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+            feedLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
+            feedLabel.setIcon(new ImageIcon("path/to/your/image.jpg")); // 실제 이미지 경로를 입력
+            userFeed.add(feedLabel);
+        }
+
+        add(imagePanel);
+        add(userFeed);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(DiaryListScreen::new);
     }
 }
