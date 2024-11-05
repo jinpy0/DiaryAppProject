043cdc62ba7edc8f6cba3f39efc5947f4b6471ca
jinpy0 <jinpyo0920@gmail.com>
2024-11-05 12:46:44 +0900

    추가

--- src/FirstTest/UI/DiaryListScreen.java
+++ src/FirstTest/UI/DiaryListScreen.java
@@ -3,14 +3,16 @@ package FirstTest.UI;
 import javax.swing.*;
 
 public class DiaryListScreen extends JFrame {
-    public DiaryListScreen() {
+    public DiaryListScreen() { // User 클래스의 인스턴스를 매개변수로 받아와서 리스트 만들기?
         setTitle("일기 목록");
         setSize(350, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        // JPanel imagePanel
+        // JPanel imagePanel 이미지 어떻게 받아올 지
+
+
 
 
 
